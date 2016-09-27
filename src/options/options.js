5b65f495ba342c38cc1748e73abccb822026708b
fengyingcai <fengyc.work@gmail.com>
2016-09-28 00:54:39 +0800

    update

--- src/options/options.js
+++ src/options/options.js
@@ -6,21 +6,74 @@ var storage = {};
 
 var urlTemp =
     "<tr><td><input type='checkbox' class='checkbox'/></td>" +
-    "<td><input type='checkbox' class='checkbox' {1} data-index='{0}'/></td>" +
-    "<td><input type='text' class='form-control' data-index='{0}' value='{2}'/></td></tr>";
+    "<td><input type='text' class='form-control' data-index='{0}' value='{2}'/></td>" +
+    "<td><input type='checkbox' class='checkbox' {1} data-index='{0}'/></td></tr>";
 
 var ruleTemp =
     "<tr><td><input type='checkbox' class='checkbox'/></td>" +
-    "<td><input type='checkbox' class='checkbox' {1} data-index='{0}'></td>" +
     "<td><input type='text' class='form-control' data-index='{0}' data-name='origin' value='{2}'/></td>" +
-    "<td><input type='text' class='form-control' data-index='{0}' data-name='target' value='{3}'/></td></tr>";
+    "<td><input type='text' class='form-control' data-index='{0}' data-name='target' value='{3}'/></td>" +
+    "<td><input type='checkbox' class='checkbox' {1} data-index='{0}'></td></tr>";
+
+/* Check all and uncheck all */
+$("table thead input[type=checkbox]").click(function () {
+    var checked = $(this).is(":checked");
+    var table = $(this).closest("table");
+    table.find("tbody tr").each(function () {
+        if (checked) {
+            $(this).find("input[type=checkbox]").first().attr("checked",'true');
+        } else {
+            $(this).find("input[type=checkbox]").first().removeAttr("checked");
+        }
+    });
+});
 
 $("#btnAddOnlineURL").click(function () {
-    $("#tblOnlineURLs tbody").append(urlTemp.format("", "", ""));
+    // already has empty row
+    var hasEmpty = false;
+    $("#tblOnlineURLs").find("input[type=text]").each(function () {
+        if ($(this).text() == "") {
+            hasEmpty = true;
+            return false;
+        }
+    });
+    if (!hasEmpty) {
+        $("#tblOnlineURLs tbody").append(urlTemp.format("", "checked", ""));
+    }
+});
+
+$("#btnDeleteOnlineURL").click(function () {
+    $("#tblOnlineURLs tbody tr").each(function () {
+        if ($(this).find("input[type=checkbox]").first().is(":checked")) {
+            $(this).remove();
+        }
+    });
 });
 
 $("#btnAddCustomRule").click(function () {
-    $("#tblCustomRules tbody").append(ruleTemp.format("", "", "", ""))
+    var hasEmpty = false;
+    $("#tblCustomRules").find("input[type=text]").each(function () {
+        if ($(this).text() == "") {
+            hasEmpty = true;
+            return false;
+        }
+    });
+    if (!hasEmpty) {
+        $("#tblCustomRules tbody").append(ruleTemp.format("", "checked", "", ""));
+    }
+});
+
+$("#btnDeleteCustomRule").click(function () {
+    $("#tblCustomRules tbody tr").each(function () {
+        if ($(this).find("input[type=checkbox]").first().is(":checked")) {
+            $(this).remove();
+        }
+    });
+});
+
+/* Save options */
+$("#btnSave").click(function () {
+
 });
 
 function displayAll() {
