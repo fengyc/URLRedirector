5a6e72aecb9f3feea80c5226280674eaae1e8e03
fengyingcai <fengyc.work@gmail.com>
2017-03-14 01:14:51 +0800

    onedrive 支持

--- src/options/onedrive.js
+++ src/options/onedrive.js
@@ -5,13 +5,16 @@
  */
 
 function OneDrive() {
-    this.CLIENT_ID = "380231ba-6b4d-44ae-a1bf-8daa67c0e15a";
-    this.AUTH_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
-    this.REDIRECT_URL = "https://urlredirector.github.io/auth_onedrive.html";
-    this.SCOPE = "files.readwrite";
-    this.RESPONSE_TYPE = "token";
-
+    this._authorizedCallbcak = null;
+    this._tab = null;
 }
+
+OneDrive.CLIENT_ID = "380231ba-6b4d-44ae-a1bf-8daa67c0e15a";
+OneDrive.AUTH_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/_authorize";
+OneDrive.REDIRECT_URL = "https://urlredirector.github.io/auth_onedrive.html";
+OneDrive.SCOPE = "onedrive.appfolder";
+OneDrive.RESPONSE_TYPE = "token";
+
 
 function _dumpQueryString(obj) {
     var queryString = "";
@@ -39,65 +42,194 @@ function _loadQueryString(queryString) {
     return obj;
 }
 
-OneDrive.prototype.authorize = function () {
+OneDrive.prototype._authorize = function (callback) {
+    if (this._tab) {
+        callback(this.isAuthorized());
+    }
+
+    this._authorizedCallbcak = callback;
+
     var queryString = _dumpQueryString({
-        client_id: this.CLIENT_ID,
-        response_type: this.RESPONSE_TYPE,
-        scope: this.SCOPE,
-        redirect_url: this.REDIRECT_URL
+        client_id: OneDrive.CLIENT_ID,
+        response_type: OneDrive.RESPONSE_TYPE,
+        scope: OneDrive.SCOPE,
+        redirect_url: OneDrive.REDIRECT_URL
     });
     var authURL = this.AUTH_URL + "?" + queryString;
+
     var creating = browser.tabs.create({
         active: true,
         url: authURL
     });
     creating.then(
         function (tab) {
-            browser.tabs.onUpdate
+            this._tab = tab;
+            // Do nothing, just wait for authorization.
         },
         function (error) {
-            console.error(error);
+            callback(false);
         }
     );
 };
 
-function authorize() {
-    var auth_url = ONEDRIVE_AUTHORIZE_URL + "?" +
-            "client_id+" + encodeURIComponent(onedrive_options.client_id) + "&" +
-            "scope=" + encodeURIComponent(onedrive_options.scope) + "&" +
-            "response_type=" + encodeURIComponent(onedrive_options.response_type) + "&" +
-            "redirect_url=" + encodeURIComponent(onedrive_options.redirect_url);
+OneDrive.prototype._handleTabUpdated = function (tabId, changeInfo, tab) {
+    if (this._tab && this._tab.id == tabId) {
+        if (changeInfo.url.startsWith(OneDrive.REDIRECT_URL)) {
+            /* Parse query string */
+            var queryString = url.split("?")[1];
+            var queryInfo = _loadQueryString(queryString);
+            /* Update obj */
+            this.access_token = queryInfo.access_token;
+            this.authentication_token = queryInfo.authentication_token;
+            this.token_type = queryInfo.token_type;
+            this.expires_in = queryInfo.expires_in;
+            this.scope = queryInfo.scope;
+            this.user_id = queryInfo.user_id;
+            this.issued_at = new Date();
+            browser.tabs.remove(tabId);
+        }
+    }
+};
 
-    var creating = browser.tabs.create({
-        active: true,
-        url: auth_url
-    });
-    creating.then(
-        function (tab) {
+OneDrive.prototype._handleTabRemoved = function(tabId, removeInfo) {
+    if (this._tab && this._tab.id == tabId) {
+        this._tab = null;
+        if (this._authorizedCallbcak) {
+            this._authorizedCallbcak(this.isAuthorized());
+        }
+    }
+};
+
+OneDrive.prototype.setup = function () {
+    /* Listen on tab update and remove  */
+    if (!browser.tabs.onUpdated.hasListener(this._handleTabUpdated)) {
+        browser.tabs.onUpdated.addListener(this._handleTabUpdated);
+    }
+    if (!browser.tabs.onRemoved.hasListener(this._handleTabRemoved)) {
+        browser.tabs.onRemoved.addListener(this._handleTabRemoved)
+    }
+};
+
+OneDrive.prototype.teardown = function () {
+    /* Remove listeners */
+    if (browser.tabs.onUpdated.hasListener(this._handleTabUpdated)) {
+        browser.tabs.onUpdated.removeListener(this._handleTabUpdated);
+    }
+    if (!browser.tabs.onRemoved.hasListener(this._handleTabRemoved)) {
+        browser.tabs.onRemoved.removeListener(this._handleTabRemoved)
+    }
+};
+
+OneDrive.prototype.isAuthorized = function() {
+    if (!this.access_token) {
+        return false;
+    }
+    var now = new Date();
+    var seconds = (now.getTime() - this.issued_at.getTime())/1000;
+    if (seconds > this.expires_in - 10) {
+        return false;
+    }
+    return true;
+};
+
+OneDrive.prototype.authorizeFromURL = function (url) {
+    if (url.startsWith(OneDrive.REDIRECT_URL)) {
+        /* Parse query string */
+        var queryString = url.split("?")[1];
+        var queryInfo = _loadQueryString(queryString);
+        /* Update obj */
+        this.access_token = queryInfo.access_token;
+        this.authentication_token = queryInfo.authentication_token;
+        this.token_type = queryInfo.token_type;
+        this.expires_in = queryInfo.expires_in;
+        this.scope = queryInfo.scope;
+        this.user_id = queryInfo.user_id;
+        this.issued_at = new Date();
+        /* Authorized promise callback */
+        if (this.authorizedPromise) {
+            this.authorizedPromise.resolve(true);
+        }
+    }
+};
+
+
+OneDrive.prototype.uploadTo = function (path, data, promise) {
+    $.ajax({
+        url: "/drive/special/approot:/" + encodeURIComponent(path) + ":/content",
+        headers: {
+            Authorization: "bearer " + this.access_token
         },
-        function (error) {
-        })
+        type: "PUT",
+        body: data,
+        success: function (data) {
+            promise.resolve(data);
+        },
+        error: function (xhr, error, e) {
+            promise.reject(error);
+        }
+    })
+};
 
-}
+OneDrive.prototype.upload = function (path, data) {
+    return new Promise(function (resolve, reject) {
+        function _upload() {
+            $.ajax({
+                url: "/drive/special/approot:/" + encodeURIComponent(path) + ":/content",
+                headers: {
+                    Authorization: "bearer " + this.access_token
+                },
+                type: "PUT",
+                body: data,
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (xhr, error, e) {
+                    reject(error);
+                }
+            })
+        }
+        if (!this.isAuthorized()) {
+            this._authorize(function (isAuthorized) {
+                if (isAuthorized) {
+                    _upload()
+                }
+                else {
+                    reject("Canceled");
+                }
+            })
+        } else {
+            _upload();
+        }
+    })
+};
 
-/**
- * Check the redirected url, if the url contains ?code= then success
- * @param url
- */
-function checkAuthorized(url) {
-    if (url.startsWith(ONEDRIVE_REDIRECT_URL)) {
-        var access_token = url.match(new RegExp("#access_token=([^&]*)($)"))
-    }
-}
-
-function handleOAuth2Callback(tabId, changeInfo, tab) {
-    if (changeInfo && changeInfo.url) {
-
-    }
-}
-
-/* Listen on tab */
-browser.tabs.onUpdated.addListener(handleOAuth2Callback());
-
-
-
+OneDrive.prototype.download = function (path) {
+    return new Promise(function (resole, reject) {
+        function _download() {
+            $.ajax({
+                url: "/drive/special/approot:/" + encodeURIComponent(path) + ":/content",
+                headers: {
+                    Authorization: "bearer " + this.access_token
+                },
+                type: "GET",
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (xhr, error, e) {
+                    reject(error);
+                }
+            });
+        }
+        if (!this.isAuthorized()) {
+            this._authorize(function (isAuthorized) {
+                if (isAuthorized) {
+                    _download();
+                } else {
+                    reject("Canceled");
+                }
+            });
+        } else {
+            _download();
+        }
+    });
+};
